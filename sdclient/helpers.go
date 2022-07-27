9aaad194aae4176db4af641d87b3f319597ef79d
Wojciech Tomaszewski <wojciech.tomaszewski1@ibm.com>
2022-07-27 14:15:18 +0200

    Add rest of regions

--- sdclient/helpers.go
+++ sdclient/helpers.go
@@ -15,6 +15,12 @@ const (
 
 var Regions = map[string]string{
 	"eu-de":    "https://eu-de.monitoring.cloud.ibm.com",
+	"eu-gb":    "https://eu-gb.monitoring.cloud.ibm.com",
 	"us-south": "https://us-south.monitoring.cloud.ibm.com",
 	"us-east":  "https://us-east.monitoring.cloud.ibm.com",
+	"ca-tor":   "https://ca-tor.monitoring.cloud.ibm.com",
+	"au-syd":   "https://au-syd.monitoring.cloud.ibm.com",
+	"jp-osa":   "https://jp-osa.monitoring.cloud.ibm.com",
+	"jp-tok":   "https://jp-tok.monitoring.cloud.ibm.com",
+	"br-sao":   "https://br-sao.monitoring.cloud.ibm.com",
 }
